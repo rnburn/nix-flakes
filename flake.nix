eea5979d371d2d4d9c7526a90998cb4ef3f24eff
Ryan Burn <ryan.burn@gmail.com>
2023-10-03 23:19:18 -0700

    work on build

--- flake.nix
+++ flake.nix
@@ -8,10 +8,15 @@
     defaultPackage.x86_64-linux =
       # Notice the reference to nixpkgs here.
       with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in stdenv.mkDerivation {
         name = "hello";
-        src = self;
-        buildPhase = "echo arf > speak";
+        src = pkgs.fetchgit {
+          url = "https://github.com/llvm/llvm-project";
+          rev = "7e856d1";
+          hash = "sha256-KxItJo+Q/oI6A7rV/0sPEVLmKzXN07k93Sege01TVR4=";
+        };
+        buildPhase = "cp README.md speak";
         installPhase = "mkdir -p $out/bin; install -t $out/bin speak";
       };
 
