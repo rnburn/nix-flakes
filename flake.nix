dcf62602ddd36db67bfd44120df52d6ca2a25c8d
rnburn <ryan.burn@gmail.com>
2023-10-11 07:14:40 +0000

    rework

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       with import nixpkgs { system = "x86_64-linux"; };
       let 
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
-	gccForLibs = gcc.cc;
+	gccForLibs = gcc13.cc;
       in stdenvNoCC.mkDerivation {
         name = "clang";
         src = pkgs.fetchgit {
@@ -25,7 +25,7 @@
           python3
         ];
 	buildInputs = [
-	  gcc
+	  gcc13
 	];
 	# configurePhase = "echo hello";
 	# buildPhase = ''
@@ -34,26 +34,26 @@
 	#   echo ${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} >> yip
 	# '';
 	# installPhase = "mkdir -p $out/bin; install -t $out/bin yip";
-	NIX_LDFLAGS="-L${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} -L${gcc.libc.dev}/lib";
-	CFLAGS="-B${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} -B${gcc.libc.dev}/lib";
+	NIX_LDFLAGS="-L${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} -L${gcc13.libc.dev}/lib";
+	CFLAGS="-B${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} -B${gcc13.libc.dev}/lib";
         configurePhase = pkgs.lib.strings.concatStringsSep " " [
           "mkdir build; cd build;"
           "cmake"
           "-G \"Unix Makefiles\""
 	  "-DGCC_INSTALL_PREFIX=${gccForLibs}"
-	  "-DC_INCLUDE_DIRS=${gcc.libc.dev}/include"
+	  "-DC_INCLUDE_DIRS=${gcc13.libc.dev}/include"
           "-DLLVM_TARGETS_TO_BUILD=\"host\""
 	  "-DLLVM_BUILTIN_TARGETS=\"x86_64-unknown-linux-gnu\""
           "-DLLVM_RUNTIME_TARGETS=\"x86_64-unknown-linux-gnu\""
           # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lld;lldb;openmp\""
-          "-DLLVM_ENABLE_PROJECTS=\"clang\""
+          "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra\""
 	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind;compiler-rt\""
 	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind\""
-	  "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi\""
+	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi\""
 	  # "-DCLANG_DEFAULT_CXX_STDLIB=\"libc++\""
-	  "-DLIBCXX_ENABLE_SHARED=OFF"
-	  "-DLIBCXX_ENABLE_STATIC=ON"
-	  "-DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=YES"
+	  # "-DLIBCXX_ENABLE_SHARED=OFF"
+	  # "-DLIBCXX_ENABLE_STATIC=ON"
+	  # "-DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=YES"
 	  # "-DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY=YES"
           # "-DCOMPILER_RT_USE_LIBCXX=ON"
           "-DCMAKE_BUILD_TYPE=Release"
