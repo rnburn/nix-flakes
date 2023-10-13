0c08a2196151154b950365898c3713a057773d58
rnburn <ryan.burn@gmail.com>
2023-10-13 05:16:46 +0000

    patch

--- flake.nix
+++ flake.nix
@@ -27,11 +27,11 @@
 	buildInputs = [
 	  gcc13
 	];
-	configurePhase = "echo hello";
-	buildPhase = ''
-	  cat clang/lib/Driver/ToolChains/Gnu.cpp >> yip
-	'';
-	installPhase = "mkdir -p $out/bin; install -t $out/bin yip";
+	# configurePhase = "echo hello";
+	# buildPhase = ''
+	#   cat clang/lib/Driver/ToolChains/Gnu.cpp >> yip
+	# '';
+	# installPhase = "mkdir -p $out/bin; install -t $out/bin yip";
 	NIX_LDFLAGS="-L${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} -L${gcc13.libc}/lib";
 	CFLAGS="-B${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} -B${gcc13.libc}/lib";
 	patches = [
@@ -41,34 +41,34 @@
 	  substituteInPlace clang/lib/Driver/ToolChains/Gnu.cpp \
 	    --replace 'GLIBC_PATH_ABC123' '${gcc13.libc}/lib'
 	'';
-        # configurePhase = pkgs.lib.strings.concatStringsSep " " [
-        #   "mkdir build; cd build;"
-        #   "cmake"
-        #   "-G \"Unix Makefiles\""
-	#   "-DGCC_INSTALL_PREFIX=${gccForLibs}"
-	#   "-DC_INCLUDE_DIRS=${gcc13.libc.dev}/include"
-        #   "-DLLVM_TARGETS_TO_BUILD=\"host\""
-	#   "-DLLVM_BUILTIN_TARGETS=\"x86_64-unknown-linux-gnu\""
-        #   "-DLLVM_RUNTIME_TARGETS=\"x86_64-unknown-linux-gnu\""
-        #   # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lld;lldb;openmp\""
-        #   # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra\""
-        #   "-DLLVM_ENABLE_PROJECTS=\"clang\""
-	#   # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind;compiler-rt\""
-	#   # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind\""
-	#   # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi\""
-	#   "-DLLVM_ENABLE_RUNTIMES=\"\""
-	#   # "-DCLANG_DEFAULT_CXX_STDLIB=\"libc++\""
-	#   # "-DLIBCXX_ENABLE_SHARED=OFF"
-	#   # "-DLIBCXX_ENABLE_STATIC=ON"
-	#   # "-DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=YES"
-	#   # "-DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY=YES"
-        #   # "-DCOMPILER_RT_USE_LIBCXX=ON"
-        #   "-DCMAKE_BUILD_TYPE=Release"
-        #   "-DCMAKE_INSTALL_PREFIX=\"$out\""
-        #   "../llvm"
-        # ];
-        # buildPhase = "make";
-        # installPhase = "make install";
+        configurePhase = pkgs.lib.strings.concatStringsSep " " [
+          "mkdir build; cd build;"
+          "cmake"
+          "-G \"Unix Makefiles\""
+	  "-DGCC_INSTALL_PREFIX=${gccForLibs}"
+	  "-DC_INCLUDE_DIRS=${gcc13.libc.dev}/include"
+          "-DLLVM_TARGETS_TO_BUILD=\"host\""
+	  "-DLLVM_BUILTIN_TARGETS=\"x86_64-unknown-linux-gnu\""
+          "-DLLVM_RUNTIME_TARGETS=\"x86_64-unknown-linux-gnu\""
+          # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lld;lldb;openmp\""
+          # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra\""
+          "-DLLVM_ENABLE_PROJECTS=\"clang\""
+	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind;compiler-rt\""
+	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind\""
+	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi\""
+	  "-DLLVM_ENABLE_RUNTIMES=\"\""
+	  # "-DCLANG_DEFAULT_CXX_STDLIB=\"libc++\""
+	  # "-DLIBCXX_ENABLE_SHARED=OFF"
+	  # "-DLIBCXX_ENABLE_STATIC=ON"
+	  # "-DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=YES"
+	  # "-DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY=YES"
+          # "-DCOMPILER_RT_USE_LIBCXX=ON"
+          "-DCMAKE_BUILD_TYPE=Release"
+          "-DCMAKE_INSTALL_PREFIX=\"$out\""
+          "../llvm"
+        ];
+        buildPhase = "make";
+        installPhase = "make install";
       };
 
   };
