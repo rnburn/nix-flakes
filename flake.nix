b6dfbc30a27f9e9aca9d6c9117ed1955b36df78e
rnburn <ryan.burn@gmail.com>
2023-10-13 05:01:03 +0000

    rework

--- flake.nix
+++ flake.nix
@@ -15,8 +15,8 @@
         name = "clang";
         src = pkgs.fetchgit {
           url = "https://github.com/llvm/llvm-project";
-          rev = "11caef0";
-          hash = "sha256-Rcmw0hMy58toxPhNithea2UH5NMvytRf4fp2WqUjLvs=";
+          rev = "4a0ccfa865437fe29ef2ecb18152df7694dddb7f";
+          hash = "sha256-ZlZKuHOdiQLkZU8uL2NiVcAgpS0sU2vR3UW88FAm39M=";
         };
         nativeBuildInputs = [
           cmake
@@ -27,43 +27,48 @@
 	buildInputs = [
 	  gcc13
 	];
-	# configurePhase = "echo hello";
-	# buildPhase = ''
-	#   echo ${gcc.libc.dev} >> yip
-	#   echo ${gccForLibs} >> yip
-	#   echo ${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} >> yip
-	# '';
-	# installPhase = "mkdir -p $out/bin; install -t $out/bin yip";
+	configurePhase = "echo hello";
+	buildPhase = ''
+	  cat clang/lib/Driver/ToolChains/Gnu.cpp >> yip
+	'';
+	installPhase = "mkdir -p $out/bin; install -t $out/bin yip";
 	NIX_LDFLAGS="-L${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} -L${gcc13.libc.dev}/lib";
 	CFLAGS="-B${gccForLibs}/lib/gcc/${targetPlatform.config}/${gccForLibs.version} -B${gcc13.libc.dev}/lib";
-        configurePhase = pkgs.lib.strings.concatStringsSep " " [
-          "mkdir build; cd build;"
-          "cmake"
-          "-G \"Unix Makefiles\""
-	  "-DGCC_INSTALL_PREFIX=${gccForLibs}"
-	  "-DC_INCLUDE_DIRS=${gcc13.libc.dev}/include"
-          "-DLLVM_TARGETS_TO_BUILD=\"host\""
-	  "-DLLVM_BUILTIN_TARGETS=\"x86_64-unknown-linux-gnu\""
-          "-DLLVM_RUNTIME_TARGETS=\"x86_64-unknown-linux-gnu\""
-          # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lld;lldb;openmp\""
-          # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra\""
-          "-DLLVM_ENABLE_PROJECTS=\"clang\""
-	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind;compiler-rt\""
-	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind\""
-	  # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi\""
-	  "-DLLVM_ENABLE_RUNTIMES=\"\""
-	  # "-DCLANG_DEFAULT_CXX_STDLIB=\"libc++\""
-	  # "-DLIBCXX_ENABLE_SHARED=OFF"
-	  # "-DLIBCXX_ENABLE_STATIC=ON"
-	  # "-DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=YES"
-	  # "-DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY=YES"
-          # "-DCOMPILER_RT_USE_LIBCXX=ON"
-          "-DCMAKE_BUILD_TYPE=Release"
-          "-DCMAKE_INSTALL_PREFIX=\"$out\""
-          "../llvm"
-        ];
-        buildPhase = "make";
-        installPhase = "make install";
+	patches = [
+	  "./clang_driver.patch"
+	];
+	postPatch = ''
+	  substituteInPlace clang/lib/Driver/ToolChains/Gnu.cpp \
+	    --replace 'GLIBC_PATH_ABC123' '${gcc13.libc.dev}/lib'
+	'';
+        # configurePhase = pkgs.lib.strings.concatStringsSep " " [
+        #   "mkdir build; cd build;"
+        #   "cmake"
+        #   "-G \"Unix Makefiles\""
+	#   "-DGCC_INSTALL_PREFIX=${gccForLibs}"
+	#   "-DC_INCLUDE_DIRS=${gcc13.libc.dev}/include"
+        #   "-DLLVM_TARGETS_TO_BUILD=\"host\""
+	#   "-DLLVM_BUILTIN_TARGETS=\"x86_64-unknown-linux-gnu\""
+        #   "-DLLVM_RUNTIME_TARGETS=\"x86_64-unknown-linux-gnu\""
+        #   # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lld;lldb;openmp\""
+        #   # "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra\""
+        #   "-DLLVM_ENABLE_PROJECTS=\"clang\""
+	#   # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind;compiler-rt\""
+	#   # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi;libunwind\""
+	#   # "-DLLVM_ENABLE_RUNTIMES=\"libcxx;libcxxabi\""
+	#   "-DLLVM_ENABLE_RUNTIMES=\"\""
+	#   # "-DCLANG_DEFAULT_CXX_STDLIB=\"libc++\""
+	#   # "-DLIBCXX_ENABLE_SHARED=OFF"
+	#   # "-DLIBCXX_ENABLE_STATIC=ON"
+	#   # "-DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=YES"
+	#   # "-DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY=YES"
+        #   # "-DCOMPILER_RT_USE_LIBCXX=ON"
+        #   "-DCMAKE_BUILD_TYPE=Release"
+        #   "-DCMAKE_INSTALL_PREFIX=\"$out\""
+        #   "../llvm"
+        # ];
+        # buildPhase = "make";
+        # installPhase = "make install";
       };
 
   };
